9df6c74527f7d9cb5864348bb78a70eba44eaf9f
azizabacc <azizabaccouri@gmail.com>
2023-11-06 13:44:57 +0100

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { ImagecomponentComponent } from './imagecomponent/imagecomponent.compone
 import { EmpInfoComponent } from './emp-info/emp-info.component';
 import { FormComponentComponent } from './form-component/form-component.component';
 import { EInfoComponent } from './e-info/e-info.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CourseContainerComponent } from './course-container/course-container.component';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { EInfoComponent } from './e-info/e-info.component';
     ImagecomponentComponent,
     EmpInfoComponent,
     FormComponentComponent,
-    EInfoComponent
+    EInfoComponent,
+    NavbarComponent,
+    FooterComponent,
+    CourseContainerComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/course-container/course-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-container',
+  templateUrl: './course-container.component.html',
+  styleUrls: ['./course-container.component.scss']
+})
+export class CourseContainerComponent {
+
+}
